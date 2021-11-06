b69b2213cc62ba05980ef86ae207767ba15b5e1e
0dminnimda <0dminnimda@gmail.com>
2021-11-07 02:19:39 +0300

    Update tst.py

--- tests/tst.py
+++ tests/tst.py
@@ -1,68 +1,4 @@
-from typing import Any, List
-from pybex.parser import parse_source
-from pybex.interpreter import interpret, eval_expr, run_interactive_mode
-from pybex.classes import Funcall, Function, Expr, Nothing, Number, String, EvalContext, Unfinished, Word
-import sys
-import traceback
-
-
-@Function.py
-def say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
-    expr: Expr
-
-    def print_expr(expr: Expr, **kwargs):
-        print(str(eval_expr(ctx, expr)), **kwargs)
-        # if isinstance(expr, Word):
-        #     print(eval_word(ctx, expr), **kwargs)
-        # else:
-        #     print(str(expr), **kwargs)
-
-    for expr in exprs[:-1]:
-        print_expr(expr, end=" ")
-    for expr in exprs[-1:]:
-        print_expr(expr, end="")
-    print()
-
-    return Nothing
-
-
-@Function.named_py("if")
-def if_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
-    if len(exprs) != 3:
-        raise ValueError("`if` expects 3 arguments")
-
-    test = eval_expr(ctx, exprs[0])
-    if not isinstance(test, Number):
-        raise TypeError("first argument for `if` should evaluate to a number")
-
-    ind = 1 + int(not test.value)  # True - 1, False - 2
-    return eval_expr(ctx, exprs[ind])
-
-
-# @Function
-# def define(ctx: EvalContext, exprs: List[Expr]) -> Expr:
-#     if len(exprs) == 2:
-#         if isinstance(exprs[0], S)
-#         ctx.namespase[]
-
-
-@Function.py
-def this(ctx: EvalContext, exprs: List[Expr]) -> Expr:
-    if len(exprs) != 1:
-        raise ValueError("`this` takes in exactly one argument")
-
-    if not isinstance(exprs[0], Funcall):
-        raise TypeError("first argument for `this` must be a function")
-
-    return exprs[0]  # we don't evaluate funcall, only return it!
-
-
-ctx = EvalContext({
-    say.name: say,
-    if_func.name: if_func,
-    this.name: this,
-    "name": "Alex"
-})
+from pybex import run_interactive_mode
 
 
 # print(result)
@@ -92,7 +28,7 @@ say(this(say(pi, 31415, this(), if)))
 
 # interpret(ctx, parse_source(program_body))
 
-run_interactive_mode(ctx)
+run_interactive_mode()
 
 
 breakpoint
