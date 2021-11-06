ad7f4a57048bacb2cce81322ae77b7bec2b30e12
0dminnimda <0dminnimda@gmail.com>
2021-11-07 02:58:47 +0300

    Create __main__.py

---
+++ pybex/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+
+from . import EvalContext, interpret, parse_source, run_interactive_mode
+
+parser = argparse.ArgumentParser(
+    prog="pybex", description="Run PyBEX programming language interpreter")
+
+parser.add_argument("file", nargs="?", help="path of the script file")
+
+args = parser.parse_args()
+
+if args.file is None:
+    run_interactive_mode()
+else:
+    with open(args.file, encoding="utf-8") as f:
+        source = f.read()
+    interpret(parse_source(source))
