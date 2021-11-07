3a487c8537665f55f024af92ed6ff90a870f46b8
0dminnimda <0dminnimda@gmail.com>
2021-11-07 03:13:42 +0300

    builtins.py: clean up

--- pybex/builtins.py
+++ pybex/builtins.py
@@ -12,10 +12,6 @@ def say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 
     def print_expr(expr: Expr, **kwargs):
         print(str(eval_expr(ctx, expr)), **kwargs)
-        # if isinstance(expr, Word):
-        #     print(eval_word(ctx, expr), **kwargs)
-        # else:
-        #     print(str(expr), **kwargs)
 
     for expr in exprs[:-1]:
         print_expr(expr, end=" ")
@@ -29,13 +25,9 @@ def say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.named_py("if")
 def if_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 3)
-    # if len(exprs) != 3:
-    #     raise ValueError("`if` expects 3 arguments")
 
     test = assert_arg_type(ctx, eval_expr(ctx, exprs[0]),
-                           0, Number, "should evaluate to a {type.__name__}")
-    # if not isinstance(test, Number):
-    #     raise TypeError("first argument for `if` should evaluate to a number")
+                           0, Number, "should evaluate to a '{type.__name__}'")
 
     ind = 1 + int(not test.value)  # True - 1, False - 2
     return eval_expr(ctx, exprs[ind])
@@ -44,17 +36,13 @@ def if_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.py
 def this(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
-    # if len(exprs) != 1:
-    #     raise ValueError("`this` takes in exactly one argument")
 
-    return exprs[0]  # we don't evaluate funcall, only return it!
+    return exprs[0]
 
 
 @Function.named_py("type")
 def type_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
-    # if len(exprs) != 1:
-    #     raise ValueError("`type` takes in exactly one argument")
 
     return String(type(exprs[0]).__name__)
 
@@ -72,13 +60,8 @@ def noeval(ctx: EvalContext, exprs: List[Expr]) -> NoReturn:
 @Function.py
 def add_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, ">=", 2)
-    # if len(exprs) < 2:
-    #     raise ValueError("`add_args` takes in at least two argument")
 
     arg1 = assert_arg_type(ctx, exprs[0], 0, Funcall)
-    # if not isinstance(exprs[0], Funcall):
-    #     raise TypeError("first argument for `add_args` must be a funcall")
-
     arg1.args.extend(exprs[1:])
 
     return arg1
@@ -87,8 +70,6 @@ def add_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.py
 def valueof(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
-    # if len(exprs) != 1:
-    #     raise ValueError("`valueof` takes in exactly one argument")
 
     cache: Set[str] = set()
     value = exprs[0]
@@ -105,30 +86,18 @@ def valueof(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.py
 def call(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, ">=", 1)
-    # if len(exprs) < 1:
-    #     raise ValueError("`call` takes in at least one argument")
 
     if isinstance(exprs[0], Funcall):
         arg1 = assert_arg_type(ctx, eval_funcall(ctx, exprs[0]), 0, Function,
-                               "should evaluate to a {type.__name__}")
+                               "should evaluate to a '{type.__name__}'")
     else:
         arg1 = assert_arg_type(ctx, valueof(ctx, exprs[:1]), 0, Function)
     return eval_expr(ctx, arg1(ctx, exprs[1:]))
-
-    # arg =
-    # if isinstance(arg, Function):
-    #     return eval_expr(ctx, arg(ctx, exprs[1:]))
-    # if isinstance(arg, String):
-    #     return eval_expr(ctx, Funcall(arg.value, exprs[1:]))
-
-    # raise TypeError("first argument for `call` must be a funcall or string")
 
 
 @Function.named_py("eval")
 def eval_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
-    # if len(exprs) != 1:
-    #     raise ValueError("`eval` takes in exactly one argument")
 
     return eval_expr(ctx, exprs[0])
 
@@ -136,12 +105,7 @@ def eval_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.py
 def assign(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 2)
-    # if len(exprs) != 2:
-    #     raise ValueError("`assign` takes in exactly two argument")
-
     arg1 = assert_arg_type(ctx, exprs[0], 0, Word)
-    # if not isinstance(arg1, Word):
-    #     raise TypeError("first argument for `assign` must be a word")
 
     arg2 = exprs[1]
     if isinstance(arg2, Funcall):
@@ -156,17 +120,8 @@ def assign(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 @Function.py
 def function(ctx: EvalContext, func_body: List[Expr]) -> Expr:
     assert_args_amount(ctx, func_body, ">=", 1)
-    # if len(exprs) < 1:
-    #     raise ValueError("`function` takes in at least one argument")
 
     args_func = assert_arg_type(ctx, func_body[0], 0, Funcall)
-    # if not isinstance(, Funcall):
-    #     raise TypeError("first argument for `function` must be a funcall")
-
-    # args_func = assert_arg_type(ctx, eval_expr(ctx, args_func), 0, Funcall)
-    # if not isinstance(args_f, Funcall):
-    #     raise TypeError("first argument for `function` must be a funcall")
-
     # validation of the args
     func_args = [assert_arg_type(ctx, expr, ind, Word)
                  for ind, expr in enumerate(args_func.args)]
@@ -186,20 +141,11 @@ def function(ctx: EvalContext, func_body: List[Expr]) -> Expr:
         ctx.pop_scope()
 
         return Nothing
-
-        # if len(exprs) != len(args):
-        #     raise ValueError("`function` takes in at least one argument")
-
     return bex_func
 
 
 @Function.py
 def args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
-    # for ind, expr in enumerate(exprs):
-    #     assert_arg_type(ctx, expr, ind, Word)
-    # if not isinstance(expr, Word):
-    #     raise TypeError("any argument for `args` must be a word")
-
     return Funcall("args", exprs)
 
 
