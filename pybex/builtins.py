249487c0793465fa4d0fb6f512d9cc4519b1ddab
0dminnimda <0dminnimda@gmail.com>
2022-01-11 18:09:26 +0300

    builtins.py: now every bex function starts with `bex_`

--- pybex/builtins.py
+++ pybex/builtins.py
@@ -3,11 +3,11 @@ from typing import List, NoReturn, Set
 from .classes import (EvalContext, Expr, Funcall, Function, Nothing, Number,
                       Scope, String, Word)
 from .interpreter import (assert_arg_type, assert_args_amount, eval_expr,
-                          eval_funcall)
+                          eval_funcall, raise_argument_error)
 
 
 @Function.py
-def say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     expr: Expr
 
     def print_expr(expr: Expr, **kwargs):
@@ -22,8 +22,8 @@ def say(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     return Nothing
 
 
-@Function.named_py("if")
-def if_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_if(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 3)
 
     test = assert_arg_type(ctx, eval_expr(ctx, exprs[0]),
@@ -34,31 +34,31 @@ def if_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 
 
 @Function.py
-def this(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_this(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     return exprs[0]
 
 
-@Function.named_py("type")
-def type_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_type(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     return String(type(exprs[0]).__name__)
 
 
-# @Function.named_py("list")
-# def list_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+# @Function.py
+# def bex_list(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 #     return Funcall("list", exprs)
 
 
 @Function.py
-def noeval(ctx: EvalContext, exprs: List[Expr]) -> NoReturn:
+def bex_noeval(ctx: EvalContext, exprs: List[Expr]) -> NoReturn:
     raise RuntimeError("`noeval` funcall should not be evaluated")
 
 
 @Function.py
-def add_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_add_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, ">=", 2)
 
     arg1 = assert_arg_type(ctx, exprs[0], 0, Funcall)
@@ -68,7 +68,7 @@ def add_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 
 
 @Function.py
-def valueof(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_valueof(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     cache: Set[str] = set()
@@ -84,7 +84,7 @@ def valueof(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 
 
 @Function.py
-def call(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_call(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, ">=", 1)
 
     if isinstance(exprs[0], Funcall):
@@ -95,15 +95,15 @@ def call(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     return arg1(ctx, exprs[1:])
 
 
-@Function.named_py("eval")
-def eval_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_eval(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     return eval_expr(ctx, exprs[0])
 
 
 @Function.py
-def assign(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_assign(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 2)
     arg1 = assert_arg_type(ctx, exprs[0], 0, Word)
 
@@ -118,7 +118,7 @@ def assign(ctx: EvalContext, exprs: List[Expr]) -> Expr:
 
 
 @Function.py
-def function(ctx: EvalContext, func_body: List[Expr]) -> Expr:
+def bex_function(ctx: EvalContext, func_body: List[Expr]) -> Expr:
     assert_args_amount(ctx, func_body, ">=", 1)
 
     args_func = assert_arg_type(ctx, func_body[0], 0, Funcall)
@@ -145,20 +145,20 @@ def function(ctx: EvalContext, func_body: List[Expr]) -> Expr:
 
 
 @Function.py
-def args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+def bex_args(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     return Funcall("args", exprs)
 
 
-@Function.named_py("exec")
-def exec_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_exec(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     for expr in exprs:
         eval_expr(ctx, expr)
 
     return Nothing
 
 
-@Function.named_py("input")
-def input_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_input(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "<=", 1)
 
     prompt = ""
@@ -168,15 +168,15 @@ def input_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     return String(input(prompt))
 
 
-@Function.named_py("repr")
-def repr_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_repr(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     return String(repr(exprs[0]))
 
 
-@Function.named_py("int")
-def int_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
+@Function.py
+def bex_int(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, "==", 1)
 
     arg1 = exprs[0]
@@ -195,21 +195,21 @@ def int_func(ctx: EvalContext, exprs: List[Expr]) -> Expr:
         f"or a Number, not '{type(arg1).__name__}'")
 
 
-builtin_scope = Scope({
-    say.name: say,
-    if_func.name: if_func,
-    this.name: this,
-    type_func.name: type_func,
-    # noeval.name: noeval,  # do we really need it?
-    add_args.name: add_args,
-    valueof.name: valueof,
-    call.name: call,
-    eval_func.name: eval_func,
-    assign.name: assign,
-    function.name: function,
-    args.name: args,
-    exec_func.name: exec_func,
-    input_func.name: input_func,
-    repr_func.name: repr_func,
-    int_func.name: int_func,
-})
+builtin_scope = Scope.from_funcions(
+    bex_say,
+    bex_if,
+    bex_this,
+    bex_type,
+    # bex_noeval,  # do we really need it?
+    bex_add_args,
+    bex_valueof,
+    bex_call,
+    bex_eval,
+    bex_assign,
+    bex_function,
+    bex_args,
+    bex_exec,
+    bex_input,
+    bex_repr,
+    bex_int,
+)
