48d789be20bb0148bd747b8752149dc60de54546
0dminnimda <0dminnimda@gmail.com>
2022-10-30 15:17:50 +0300

    conditions.py: format

--- pybex/builtins/conditions.py
+++ pybex/builtins/conditions.py
@@ -9,8 +9,13 @@ def bex_if(ctx: EvalContext, exprs: List[Expr]) -> Expr:
     assert_args_amount(ctx, exprs, ">=", 2)
     assert_args_amount(ctx, exprs, "<=", 3)
 
-    test = assert_arg_type(ctx, eval_expr(ctx, exprs[0]),
-                           0, Number, "should evaluate to a `{type.__name__}`")
+    test = assert_arg_type(
+        ctx,
+        eval_expr(ctx, exprs[0]),
+        0,
+        Number,
+        "should evaluate to a `{type.__name__}`",
+    )
 
     if test.value:
         return eval_expr(ctx, exprs[1])
