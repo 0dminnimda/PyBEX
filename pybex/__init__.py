9e1831d2c0c9d19cdeb6cf009ab6672f8e01da99
0dminnimda <0dminnimda@gmail.com>
2022-10-31 01:51:50 +0300

    __init__.py: import all usefull names

--- pybex/__init__.py
+++ pybex/__init__.py
@@ -2,6 +2,24 @@ __version__ = "0.0.2"
 __name__ = "pybex"
 
 from .builtins import builtin_scope
-from .classes import EvalContext, Scope
-from .run import interpret, run_interactive_mode
+from .classes import (
+    EvalContext,
+    Expr,
+    Funcall,
+    Function,
+    Nothing,
+    Number,
+    Scope,
+    String,
+    Word,
+)
+from .interpreter import (
+    assert_arg_type,
+    assert_args_amount,
+    eval_expr,
+    eval_funcall,
+    eval_word,
+    raise_argument_error,
+)
 from .parser import parse_source
+from .run import interpret, run_interactive_mode
