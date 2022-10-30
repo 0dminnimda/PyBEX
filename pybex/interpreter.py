9ec035453ef12ecd3cf6615d196dbbab4bf4bea0
0dminnimda <0dminnimda@gmail.com>
2022-10-30 09:32:11 +0300

    interpreter.py: remove commented out num_of_args_to_word

--- pybex/interpreter.py
+++ pybex/interpreter.py
@@ -2,22 +2,6 @@ from typing import Any, List, Optional, Type, TypeVar, NoReturn
 
 from .classes import EvalContext, Expr, Funcall, Function, Word
 
-# import warnings
-# def num_of_args_to_word(number: int) -> str:
-#     assert number >= 0
-#     words = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-#              6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-#              11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
-#              15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
-#              19: 'nineteen', 20: 'twenty'}
-
-#     if number < 21:
-#         return words[number]
-#     else:
-#         warnings.warn("PyBEX don't support crazy functions "
-#                       "with more than 20 arguments")
-#         return "too many (over 20)"
-
 
 T = TypeVar("T")
 
